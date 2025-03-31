a785108c32b7d6c06d2c3592646f1571c544133a
ghrisss <heriquebati@gmail.com>
2025-03-31 21:26:19 +0000

    chore: roteiro simples do projeto e import dos dados em csv

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# importar e tratar as bases de dados    
+emails_pd = pd.read_csv(r"database/emails.csv")
+stores_pd = pd.read_csv(r"database/stores.csv", encoding="unicode_escape")
+sales_pd = pd.read_csv(r"database/sales.csv")
+
+# calcular indicadores desejados
+
+# mandar os OnePages para cada gerente de loja respectivo
+
+# salvar um arquivo de backup em uma pasta
+
+# mandar um email separado para a diretoria com tudo
