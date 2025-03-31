b524f37cc08c0a065835833e0d94550fe5fc3cc3
ghrisss <heriquebati@gmail.com>
2025-03-31 21:27:02 +0000

    fix: adicao de separator necessário para correta leitura do csv

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # importar e tratar as bases de dados    
 emails_pd = pd.read_csv(r"database/emails.csv")
-stores_pd = pd.read_csv(r"database/stores.csv", encoding="unicode_escape")
+stores_pd = pd.read_csv(r"database/stores.csv", encoding="unicode_escape", sep=";")
 sales_pd = pd.read_csv(r"database/sales.csv")
 
 # calcular indicadores desejados
