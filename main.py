7f54c2055e39adf2c3c1f88b145dbba35b0cc9d7
ghrisss <heriquebati@gmail.com>
2025-04-01 21:31:37 +0000

    chore: criando um dicionario com dataframes separados para cada loja

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
-# importar e tratar as bases de dados    
+# importar e tratar as bases de dados
 emails_pd = pd.read_csv(r"database/emails.csv")
 stores_pd = pd.read_csv(r"database/stores.csv", encoding="unicode_escape", sep=";")
 sales_pd = pd.read_csv(r"database/sales.csv")
 
 # calcular indicadores desejados
+
+sales_pd = sales_pd.merge(stores_pd, on="Store ID")
+dict_stores = {
+    store: sales_pd.loc[sales_pd["Store"] == store, :] for store in stores_pd["Store"]
+}
 
 # mandar os OnePages para cada gerente de loja respectivo
 
