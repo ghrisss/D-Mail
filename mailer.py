4f2b716a9db5b19904cf07df47bbb07e5ab79019
ghrisss <heriquebati@gmail.com>
2025-04-13 01:39:09 +0000

    refac: preparando arquivo mailer para envio de arquivo a diretoria, mensagens e assunto diferentes

--- mailer.py
+++ mailer.py
@@ -4,21 +4,18 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pathlib import Path
 
-from pandas._libs.tslibs.timestamps import Timestamp
-
 from configs import DEBUG, EMAIL_PASSWORD
 
 
 def send_email(
     email_to: str,
-    name_to: str,
-    today: Timestamp,
+    subject: str,
     store: str,
     body_text: str,
     file_to_attach: Path,
 ):
     msg = MIMEMultipart()
-    msg["Subject"] = f"OnePage Dia {today.day}/{today.month} - Loja {store}"
+    msg["Subject"] = subject
     msg["From"] = "seuemail@gmail.com"
     msg["To"] = f"{email_to}"
 
@@ -33,7 +30,15 @@ def send_email(
         servidor.send_message(msg)
         servidor.quit()
         if DEBUG:
-            print(f"\u2713  Email enviado ao gerente {name_to} da loja {store}")
+            print(
+                f"\u2713  Email da loja {store} enviado"
+            ) if store is not None else print(
+                "\u2713  Email da Diretoria enviado"
+            )
     except FileNotFoundError as err:
         print(f"Erro ao busca o arquivo backup em anexo {err}")
-        print(f"Abortando operacao de envio referente a loja {store}")
+        print(
+            f"Abortando operacao de envio referente a loja {store}"
+        ) if store is not None else print(
+            "Abortando operacao de envio para a diretoria"
+        )
